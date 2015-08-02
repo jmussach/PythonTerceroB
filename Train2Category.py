56961353ea6807d839fbee8dce701511d5d38ea4
david <incioqk@gmail.com>
2015-08-02 11:42:42 +0200

    Saca Regiones sobre prueba de 50 filas. Puntos cercanos a la costa no los clasifica.

--- Train2Category.py
+++ Train2Category.py
@@ -1,35 +1,36 @@
+def AddRegion2LanLongData(LanLogData,GeojsonRegion)
+
 __author__ = 'david'
 __author__ = 'david'
 
 from shapely.geometry import shape,Point
 import pygeoj
 import pandas as pd
+import numpy as np
 
-DatosTrain=pd.read_csv("data/DatosEntrenFiltrados.csv",index_col=0)
-
-#Seleccionamos las columnas de longitud y latitud
-LongLat=DatosTrain.loc[:,['lng','lat']]
-
-for i in range(1,len(LongLat.index)):
-    PuntoCoord=LongLat.iloc[i]
-    print PuntoCoord
-    Lat=PuntoCoord.values[0]
-    Lng=PuntoCoord.values[1]
-    print Lat," y ",Lng
-    if i==10:
-        break
-
-
+DatosTrain=pd.read_csv("data/DatosEntrenFiltrados50.csv",index_col=0)
 
 js = pygeoj.load("data/spain-communities.geojson")
 
-point1 = Point(Lng,Lat)
+#Seleccionamos las columnas de longitud y latitud
+LongLat=DatosTrain.loc[:,['lng','lat']]
+DatosTrain["Region"]=pd.Series(np.nan,index=DatosTrain.index)
 
+for i in range(0,len(LongLat.index)):
+    PuntoCoord=LongLat.iloc[i]
+    Lng=PuntoCoord.values[0]
+    Lat=PuntoCoord.values[1]
+    #print Lat," y ",Lng
+
+    point1 = Point(Lng,Lat)
 
 # check each polygon to see if it contains the point
-for feature in js:
-    polygon = shape(feature.geometry)
-    print polygon.contains(point1)
-    if polygon.contains(point1):
-        print 'Found containing polygon:', feature.properties['name']
+    for feature in js:
+        polygon = shape(feature.geometry)
+        if polygon.contains(point1):
+           # print 'Found containing polygon:', feature.properties['name']
+            DatosTrain.iloc[i,(len(DatosTrain.columns)-1)]=feature.properties['name']
+            #print DatosTrain.loc[i,['Region']]," y ", feature.properties['name']
+    print i
 
+return DatosTrain.to_csv("Prueba4.csv",sep=";",encoding="UTF-8")
