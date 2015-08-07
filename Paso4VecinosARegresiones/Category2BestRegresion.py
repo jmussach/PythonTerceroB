ba78c8187b95ba759fa5846b0001511aaf910064
david <incioqk@gmail.com>
2015-08-07 13:05:24 +0200

    Paso 4: Funcion para calcular la mejor regresion a aplicar a uno de los datos a valorar

--- Paso4VecinosARegresiones/Category2BestRegresion.py
+++ Paso4VecinosARegresiones/Category2BestRegresion.py
@@ -1,9 +1,10 @@
 __author__ = 'david'
-# -*- coding: utf-8 -*-
+# coding=utf-8
 from VecinosKenCSVcategorize import punto2vecinosk
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import sklearn.neighbors as sk
 
 DatosAValorar=pd.read_csv("DataInput/DatosAValorarConRegion.csv",sep=";",index_col=0,encoding="UTF-8")
 
@@ -11,15 +12,19 @@ DatosAValorar.iloc[0]
 #Vecinos=punto2vecinosk(InmuebleAValorar,6)
 
 PuntoDic=DatosAValorar.iloc[0]
-CSVdireccion="DataCategorize/"+PuntoDic.iloc[0]['Region']+"_"+PuntoDic.iloc[0]['type']+"_"+PuntoDic.iloc[0]['subType']+".csv"
+CSVdireccion="DataCategorize/"+PuntoDic['Region']+"_"+PuntoDic['type']+"_"+PuntoDic['subType']+".csv"
 Datos=pd.read_csv(CSVdireccion,sep=";",index_col=0)
 
 LatLong=Datos.loc[:,['lat','lng']].values
 #Calculamos la distancia de todos con todos para cada categoria BallTree con Haversine
 tree =sk.BallTree(LatLong,metric="haversine")#Calcula la distancia Habersine de todos los puntos entre si
 #y los ordena en estructura de arbol
-dist, ind = tree.query(Datos[0], k=6)
-Datos.iloc[ind[0]]
+MedianVec=[]
+for i in range(len(LatLong)):
+    dist, ind = tree.query(LatLong[i], k=6)
+
+    MedianVec.append(np.median(Datos.iloc[ind[0]]['price']))
+
 
 T=len(Datos)
 Tr=round(0.75*T) #Muestra de entrenamiento
@@ -31,37 +36,33 @@ priceTs=Datos.iloc[Tr+1:T]['price']
 #Suma de los datos de planta de los datos de entrenamiento
 Splanta=np.sum(Datos.iloc[0:Tr]['planta'])
 
-#Calculo de la mediana de los precios de los kvecinos
-Vecinos=punto2vecinosk(Datos.loc[:,'price'],6)
-medianvec=np.median(Vecinos..values[0])
 
 #genero las variables en desviaciones con sus medianas:
 #Desv m2:
 
-desvm2=Datos.loc[:,'gross.size'].values-np.median(Vecinos.loc[:,'gross.size'])
+desvm2=Datos.loc[:,'gross.size'].values-np.median(Datos.iloc[ind[0]]['gross.size'])
 #desvroom=rooms-np.median(rooms[index_vecinos], axis=1)
-desvroom=Datos.loc[:,'rooms'].values-np.median(Vecinos.loc[:,'rooms'])
+desvroom=Datos.loc[:,'rooms'].values-np.median(Datos.iloc[ind[0]]['rooms'])
 
 if Splanta > 0:
-    desvplanta=Datos.loc[:,'planta'].values-np.median(Vecinos.loc[:,'planta'])
+    desvplanta=Datos.loc[:,'planta'].values-np.median(Datos.iloc[ind[0]]['planta'])
 else:
     desvplanta=0
 
 #genero la primera matriz de regresores donde utilizamos la mediana de los
 #seis vecinos las variables hedonicas y sus interacciones
 
-MedianVec=[medianvec]*len(Datos)#Columna con el precio mediano repetido
 
 REG_1=np.column_stack((MedianVec,
                        Datos.loc[:,'gross.size'].values,
                        Datos.loc[:,'rooms'].values,
                        MedianVec*Datos.loc[:,'gross.size'].values,
                        MedianVec*Datos.loc[:,'rooms'].values,
-                       Datos.loc[:,'gross.size'].values*medianvec*Datos.loc[:,'rooms'].values))
+                       Datos.loc[:,'gross.size'].values*MedianVec*Datos.loc[:,'rooms'].values))
 
 
 
-# # estimo los parámetros
+# # estimo los parametros
 param_1=sm.OLS(priceTr,REG_1[:Tr,:]).fit().params
 priceEstAll_1=np.dot(REG_1,param_1)
 #
@@ -80,12 +81,12 @@ if Splanta > 0:
                        MedianVec*Datos.loc[:,'gross.size'].values,
                        MedianVec*Datos.loc[:,'rooms'].values,
                        MedianVec*Datos.loc[:,'planta'].values,
-                       Datos.loc[:,'gross.size'].values*medianvec*Datos.loc[:,'rooms'].values,
-                       Datos.loc[:,'gross.size'].values*medianvec*Datos.loc[:,'planta'].values,
-                       Datos.loc[:,'planta'].values*medianvec*Datos.loc[:,'rooms'].values))
+                       Datos.loc[:,'gross.size'].values*MedianVec*Datos.loc[:,'rooms'].values,
+                       Datos.loc[:,'gross.size'].values*MedianVec*Datos.loc[:,'planta'].values,
+                       Datos.loc[:,'planta'].values*MedianVec*Datos.loc[:,'rooms'].values))
 
 
-    # estimo los parámetros
+    # estimo los parametros
     param_1p=sm.OLS(priceTr,REG_1p[:Tr,:]).fit().params
     priceEstAll_1p=np.dot(REG_1p,param_1p)
 
@@ -120,7 +121,7 @@ for i in range(T):
     if (Splanta > 0 and desvplanta[i] < 0):
         desvplantamin[i] = desvplanta[i]
 
-    ####utilizo como regresores la previsión del modelo 1 y las desviaciones
+    ####utilizo como regresores la prevision del modelo 1 y las desviaciones
 REG_2=np.column_stack((priceEstAll_1,
     desvm2plus,desvm2min,
     desvroomplus,desvroommin))
@@ -174,10 +175,10 @@ else:
 metrics_dict = {"errorpc_1" : errorpc_1, "errorpc_1p" : errorpc_1p,
                 "errorpc_2" : errorpc_2, "errorpc_2p" : errorpc_2p,
                 "size" : len(Datos),
-                 "train" : Tr, "test" : (T-Tr),
-                "Region" :PuntoDic.iloc[0]['Region'],
-                "type" : PuntoDic.iloc[0]['type'],
-                "subt" : PuntoDic.iloc[0]['subType']}
+                "train" : Tr, "test" : (T-Tr),
+                "Region" :PuntoDic['Region'],
+                "type" : PuntoDic['type'],
+                "subt" : PuntoDic['subType']}
 
 
 print param_models[index_err[0]]
